8af5270a3cccbfd12965c22f1117d89ad5f65aa8
SalkinF <niklas.fahning@dci-student.org>
2024-11-25 12:36:36 +0100

    delete 2

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 terraform {
   backend "s3" {
-    bucket = "niklas-git-codebuild-terraform"
+    bucket = "sharanya-git-codebuild-terraform"
     key = "terraform.tfstate"
     region= "eu-central-1"
   }
@@ -17,19 +17,19 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.16.0"
 
-  name= "Niklas-VPC"
+  name= "Sharanya-VPC"
   cidr = "10.0.0.0/16"
   azs             = ["eu-central-1a"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
   public_subnet_tags = {
-    Name = "Niklas-Public-Subnet"
+    Name = "Sharanya-Public-Subnet"
   }
   private_subnet_tags = {
-    Name = "Niklas-Private-Subnet"
+    Name = "Sharanya-Private-Subnet"
   }
   tags = {
-    Owner= "Niklas"
+    Owner= "Sharanya"
   }
 }
